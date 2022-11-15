bafbd22de486c098b0237b8e2756b8512934a5a0
FAISAL <85350108+faisal-kursheedali@users.noreply.github.com>
2022-11-15 08:10:43 +0000

    worked on screening

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,19 @@
-import tsImg from "./ts.svg";
-import "./App.css"
+import tsImg from './ts.svg'
+import './App.css'
 
 export const App = () => {
   return (
     <div className="App">
-      <img src={tsImg } alt="" className="App-ts-img" />
-      <div className="App-head">
-        Typescript starter kit
+      <img src={tsImg} alt="" className="App-ts-img" />
+      <div className="App-head">Typescript starter kit</div>
+      <div className="App-sub">
+        Github Repository{' '}
+        <a
+          href="https://github.com/faisal-kursheedali/webpack-starter"
+          className="App-sub-link"
+        >
+          link
+        </a>
       </div>
     </div>
   )
